b9a716c05e04bec02eb9ef6ab1b03593e05d31ae
Antoine NEBOUT <Antoine.NEBOUT@protonmail.com>
2025-07-14 18:03:43 +0200

    First commit

---
+++ src/main/java/fr/antoinenebout/vacation_app_api/VacationAppApiApplication.java
@@ -0,0 +1,13 @@
+package fr.antoinenebout.vacation_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationAppApiApplication.class, args);
+    }
+
+}
